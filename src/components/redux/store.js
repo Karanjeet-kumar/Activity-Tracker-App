45df1a833426ee849581e94f77248fbf98df6ae3
Karanjeet-kumar <karanrajput8400@gmail.com>
2025-06-08 12:32:20 +0530

    Persisting Our ReduxStore

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authSlice,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "activity-tracker-store",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authSlice,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./components/redux/store";
+import store from "./components/redux/store";
 import { Toaster } from "sonner";
 import { NavProvider } from "./components/context/NavContext";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <NavProvider>
-        <App />
-      </NavProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavProvider>
+          <App />
+        </NavProvider>
+        <Toaster richColors />
+      </PersistGate>
     </Provider>
-    <Toaster richColors />
   </StrictMode>
 );
